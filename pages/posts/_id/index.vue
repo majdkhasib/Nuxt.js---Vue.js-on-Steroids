080a7e37bbe94808bce18da36d4e15002e82004a
Majd Khasib <majdkhasib@cloud.upwork.com>
2022-04-12 18:33:57 +0300

    feat:Adding single post preview

--- pages/posts/_id/index.vue
+++ pages/posts/_id/index.vue
@@ -1,6 +1,24 @@
 <template>
-  <div class="single-post-page">Single post</div>
+  <base-card class="single-post-page">
+    <h1>{{ post.title }}</h1>
+    <p>{{ post.previewText }}</p>
+  </base-card>
 </template>
+<script>
+import PostPreview from "~/components/Posts/PostPreview.vue";
+export default {
+  components: { PostPreview },
+  computed: {
+    post() {
+      const postId = this.$route.params.id;
+      const posts = this.$store.getters.posts;
+      const post = posts.find((post) => post.id == postId);
+
+      return post;
+    },
+  },
+};
+</script>
 
 <style scoped>
 </style>
