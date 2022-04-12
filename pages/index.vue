88c18a606efaec997431ba1345b58b724dcd5fa1
Majd Khasib <majdkhasib@cloud.upwork.com>
2022-04-12 17:19:16 +0300

    feat:Adding PostPreview component

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,69 @@
 <template>
-  <h1>Home page</h1>
+  <div class="home-page">
+    <section class="intro">
+      <h1>Get the latest tech news!</h1>
+    </section>
+    <section class="featured-posts">
+      <PostPreview
+        v-for="post in posts"
+        :key="post.id"
+        :id="post.id"
+        :thumbnail="post.thumbnail"
+        :title="post.title"
+        :previewText="post.previewText"
+      />
+    </section>
+  </div>
 </template>
 
 <script>
-export default {};
-</script>
+import PostPreview from "@/components/Posts/PostPreview";
 
-<style>
+export default {
+  components: {
+    PostPreview,
+  },
+  data() {
+    return {
+      posts: [
+        {
+          id: "1",
+          thumbnail:
+            "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg",
+          title: "Hello there!",
+          previewText: "This my first post!",
+        },
+        {
+          id: "2",
+          thumbnail:
+            "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg",
+          title: "Hello there - the second time!",
+          previewText: "This my second post!",
+        },
+        {
+          id: "3",
+          thumbnail:
+            "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg",
+          title: "Hello there - the third time!",
+          previewText: "This my third post!",
+        },
+      ],
+    };
+  },
+};
+</script>
+<style scoped>
+.intro {
+  padding: 1em;
+  background: rgb(136, 221, 203);
+  text-align: center;
+}
+
+.featured-posts {
+  display: flex;
+  padding: 1em;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
 </style>
